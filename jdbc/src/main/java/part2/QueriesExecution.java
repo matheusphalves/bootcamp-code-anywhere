6e9fef472ef682dd04b8c06a249069bc0c5a8c4b
Matheus Phelipe <matheusphalves@gmail.com>
2021-05-21 20:16:00 -0300

    added QueriesExecution class

--- jdbc/src/main/java/part2/QueriesExecution.java
+++ jdbc/src/main/java/part2/QueriesExecution.java
@@ -1,4 +1,18 @@
 package part2;
 
 public class QueriesExecution {
+
+    public static void main(String[] args) {
+        AlunoDAO alunoDAO = new AlunoDAO();
+
+        System.out.println(alunoDAO.list());
+        System.out.println(alunoDAO.getById(2));
+        //alunoDAO.create(new Aluno("Joel", 50, "SC"));
+        //alunoDAO.delete(4);
+        Aluno alunoNovo = alunoDAO.getById(1);
+        alunoNovo.setNome("Jéssica");
+        alunoNovo.setIdade(25);
+        alunoNovo.setEstado("MT");
+        alunoDAO.update(alunoNovo);
+    }
 }
