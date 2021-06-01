8d82d751c81effa75e226058d672aa0408de61d6
Matheus Phelipe <matheusphalves@gmail.com>
2021-05-31 21:05:57 -0300

    created PersonService

--- src/main/java/one/digital/innovation/personapi/service/PersonService.java
+++ src/main/java/one/digital/innovation/personapi/service/PersonService.java
@@ -1,2 +1,26 @@
-package one.digital.innovation.personapi.service;public class PersonService {
+package one.digital.innovation.personapi.service;
+
+import one.digital.innovation.personapi.dto.response.MessageResponseDTO;
+import one.digital.innovation.personapi.entity.Person;
+import one.digital.innovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+    @Autowired //injeção de dependencia - no construtor facilita criação de testes unitários
+    public PersonService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+    public MessageResponseDTO createPerson(Person person){
+        Person savedPerson = this.personRepository.save(person);
+        return MessageResponseDTO
+                .builder()
+                .message("Created person with ID " + savedPerson.getId())
+                .build();
+    }
+
 }
