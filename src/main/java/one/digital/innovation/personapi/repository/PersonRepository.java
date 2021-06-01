d1d886ae192a644c33101ae194a02a4a14cab13a
Matheus Phelipe <matheusphalves@gmail.com>
2021-05-31 21:06:21 -0300

    created PersonRepository

--- src/main/java/one/digital/innovation/personapi/repository/PersonRepository.java
+++ src/main/java/one/digital/innovation/personapi/repository/PersonRepository.java
@@ -1,2 +1,8 @@
-package one.digital.innovation.personapi.repository;public interface PersonRepository {
+package one.digital.innovation.personapi.repository;
+
+import one.digital.innovation.personapi.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
 }
