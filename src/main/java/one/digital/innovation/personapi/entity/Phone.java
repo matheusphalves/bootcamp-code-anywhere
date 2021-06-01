f8910eaca60344b8da8176d701799ca15753cb93
Matheus Phelipe <matheusphalves@gmail.com>
2021-05-31 21:06:45 -0300

    added entity Person, Phone and enum PhoneType

--- src/main/java/one/digital/innovation/personapi/entity/Phone.java
+++ src/main/java/one/digital/innovation/personapi/entity/Phone.java
@@ -1,2 +1,26 @@
-package one.digital.innovation.personapi.entity;public class Phone {
+package one.digital.innovation.personapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digital.innovation.personapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+@Entity
+
+@Data //get e set
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //gera id de forma incremental
+    private long id;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false) //obrigatório
+    private PhoneType type;
+    @Column(nullable = false) //obrigatório
+    private String number;
 }
