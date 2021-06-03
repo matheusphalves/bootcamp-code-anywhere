1b95557c44b6199d4d3590dad430dddac2c14676
Matheus Phelipe <matheusphalves@gmail.com>
2021-06-03 20:20:35 -0300

    created list all people functionality

--- src/main/java/one/digital/innovation/personapi/controller/PersonController.java
+++ src/main/java/one/digital/innovation/personapi/controller/PersonController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -24,6 +25,10 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
         return this.personService.createPerson(personDTO);
+    }
+    @GetMapping("/list")
+    public List<PersonDTO> listAll(){
+        return this.personService.listAll();
     }
 
 
